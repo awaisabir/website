4c3ce9065cb5c42e4c13ef1e7c604c8c09a02a18
awaisabir <awaisabirqureshi@gmail.com>
2017-05-18 00:59:45 -0400

    Added scroll effect on the navbar

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -18,9 +18,17 @@ $(document).ready(function() {
         let scroll = $(window).scrollTop();
 
         if (scroll >= 350) {
+            $(".navigation").find('img').css({ height: '75px' });
+            $(".navigation").css({ height: '100px' });
             $(".navigation").addClass("white");
         } else {
             $(".navigation").removeClass("white");
+            $(".navigation").find('img').css({
+                height: '100px'
+            });
+            $(".navigation").css({
+                height: '150px'
+            });
         }
     });
 });
