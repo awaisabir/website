7f525e442f351ffbdd2c80e65d93f26e0e7c859e
awaisabir <awaisabirqureshi@gmail.com>
2017-08-08 12:01:45 -0600

    Hamburger menu interactivity

--- public/scripts/hamburger.js
+++ public/scripts/hamburger.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     $('#nav-icon1').click(function() {
-        // future side bar
-    });
+        $('.sidenav').animate({width: '250px'})
+        // $(this).css({'display': 'none'})
+        $('.navigation').css({'z-index': '0'})
+    })
+
+    $('.closebtn').click(function() {
+        $('.sidenav').css({'width': '0'})
+        // $('#nav-icon1').css({'display': 'flex', 'z-index': '0'})       
+    })
+
+    $('.closebtn a').click(function() {
+        $('.sidenav').css({'width': '0'})
+        $('.navigation').css({'z-index': '1'})        
+        // $('#nav-icon1').css({'display': 'flex', 'z-index': '0'})       
+    })
+
 });
+
