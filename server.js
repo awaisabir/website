62fb9277365099224b7cf2069f7e881573c99979
root <root@www.awaisabir.com>
2017-12-22 22:22:27 +0000

    changed port for hosting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 let app = express()
 
 app.use(express.static(__dirname + '/public'))
