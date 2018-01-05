ad8031b042813d735309df2158a7dad071c12bed
awaisabir <awaisqureshi@cmail.carleton.ca>
2018-01-05 18:59:19 +0000

    Semicolons are a necessity

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 80
-let app = express()
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 80;
+let app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'))
-})
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/projects.html'))
-})
+    res.sendFile(path.join(__dirname + '/views/projects.html'));
+});
 
 app.get('/resume', (req, res) => {
-    res.contentType('application/pdf')
-    res.sendFile(path.join(__dirname + '/public/resume/Resume.pdf'))
-})
+    res.contentType('application/pdf');
+    res.sendFile(path.join(__dirname + '/public/resume/Resume.pdf'));
+});
 
 app.get('*', (req, res) => {
-    res.send('404')
-})
+    res.send('404');
+});
 
 app.listen(PORT, err => {
     if (err)
-        console.log(err)
+        console.log(err);
     else
-        console.log(`Server started`)
-})
+        console.log(`Server started`);
+});
